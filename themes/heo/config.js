6e2b8bd839a38c0be70f141c780c378a6e5af5fa
baily <104406397+bailylu@users.noreply.github.com>
2024-08-03 22:08:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -3,7 +3,11 @@ const CONFIG = {
 
   HEO_SITE_CREATE_TIME: '2024-08-1', // 建站日期，用于计算网站运行的第几天
 
-  
+  // 首页顶部通知条滚动内容，如不需要可以留空 []
+ // NOTICE_BAR: [
+ //   { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+ //   { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+ //  ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: true,
