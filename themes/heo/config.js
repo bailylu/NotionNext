243d54dc77ec7dc6179e4aaed7f7974718f48826
baily <104406397+bailylu@users.noreply.github.com>
2024-08-03 20:40:05 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '生活',
   HEO_HERO_TITLE_2: '科技价值',
-  HEO_HERO_TITLE_3: 'https://baily.life/',
+  HEO_HERO_TITLE_3: 'baily.life',
   HEO_HERO_TITLE_4: '我的小店',
   HEO_HERO_TITLE_5: '贝利小铺',
   HEO_HERO_TITLE_LINK: 'https://shop.baily.life/',
@@ -41,9 +41,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL2: 'https://baily.life',
+  HEO_INFO_CARD_ICON2: 'fab fa-wechat',
+  HEO_INFO_CARD_URL3: 'https://baily.life',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -107,10 +107,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '公众号',
+  HEO_SOCIAL_CARD_TITLE_2: '快人一步获取最新文章',
+  HEO_SOCIAL_CARD_TITLE_3: '点击关注',
+  HEO_SOCIAL_CARD_URL: 'https://mp.weixin.qq.com/s?__biz=Mzg2MDg3NTk0MQ==&mid=2247483677&idx=1&sn=b2b6c610a97d18d7684e90d491ed935b&chksm=ce1efcd7f96975c1a04994bc00181ec349853be6fdf99aef84cf2cbe44fb5b9042bb2376a194&token=9436536&lang=zh_CN#rd',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
