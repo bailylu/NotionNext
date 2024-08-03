ada1896b88d1707bced196621403559b2f219645
baily <104406397+bailylu@users.noreply.github.com>
2024-08-03 20:06:33 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -11,12 +11,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '生活',
+  HEO_HERO_TITLE_2: '科技价值',
+  HEO_HERO_TITLE_3: 'baily.life',
+  HEO_HERO_TITLE_4: '我的小店',
+  HEO_HERO_TITLE_5: '看看有没有你需要的',
+  HEO_HERO_TITLE_LINK: 'shop.baily.life',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
