635c833146fa70d359a0384ad74b85b2168a64e2
baily <104406397+bailylu@users.noreply.github.com>
2024-08-09 07:26:36 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -15,7 +15,7 @@ const CONFIG = {
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: '开始查询', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL:
-    'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
+    'https://school.izanoffer.com', // 英雄区按钮
   STARTER_HERO_BUTTON_2_TEXT: '来论坛一起讨论吧', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
