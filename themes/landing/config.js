0ee7aef6f78bd543e0a93dcf7fccb59b66318172
baily <104406397+bailylu@users.noreply.github.com>
2024-08-08 21:31:54 +0800

    Update config.js

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -1,22 +1,22 @@
 const CONFIG = {
 
-  LANDING_HEADER_BUTTON_1_TITLE: 'Github开源',
-  LANDING_HEADER_BUTTON_1_URL: 'https://github.com/tangly1024/NotionNext',
+  LANDING_HEADER_BUTTON_1_TITLE: '我的life记录',
+  LANDING_HEADER_BUTTON_1_URL: 'https://vlog.baily.life',
 
-  LANDING_HEADER_BUTTON_2_TITLE: '作者博客',
-  LANDING_HEADER_BUTTON_2_URL: 'https://blog.tangly1024.com/',
+  LANDING_HEADER_BUTTON_2_TITLE: '我的博客',
+  LANDING_HEADER_BUTTON_2_URL: 'https://blog.baily.Life/',
 
   // 首页大图英雄板块
-  LANDING_HERO_TITLE_1: 'NotionNext',
-  LANDING_HERO_P_1: '快速搭建独立站、轻松放大品牌价值！',
-  LANDING_HERO_BUTTON_1_TEXT: '开始体验',
-  LANDING_HERO_BUTTON_1_LINK: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next',
-  LANDING_HERO_BUTTON_2_TEXT: '了解更多',
-  LANDING_HERO_BUTTON_2_LINK: 'https://docs.tangly1024.com/about',
-  LANDING_HERO_VIDEO_IMAGE: '/images/home.png',
+  LANDING_HERO_TITLE_1: '贝利说TV',
+  LANDING_HERO_P_1: 'Tech Value | 科技/生活/性价比',
+  LANDING_HERO_BUTTON_1_TEXT: '好物推荐',
+  LANDING_HERO_BUTTON_1_LINK: 'https://muselink.cc/bailytalk',
+  LANDING_HERO_BUTTON_2_TEXT: '我的小铺',
+  LANDING_HERO_BUTTON_2_LINK: 'https://shop.baily.life',
+  LANDING_HERO_VIDEO_IMAGE: '/images/资源_160.png',
   //   HERO_VIDEO_URL: '/videos/video.mp4',
-  LANDING_HERO_VIDEO_IFRAME: '//player.bilibili.com/player.html?aid=913088616&bvid=BV1fM4y1L7Qi&cid=1187218697&page=1&&high_quality=1',
-  LANDING_HERO_VIDEO_TIPS: 'Watch the full video (2 min)',
+  LANDING_HERO_VIDEO_IFRAME: 'https://player.bilibili.com/player.html?isOutside=true&aid=81706629&bvid=BV1GJ41187fG&cid=139803839&p=1',
+  LANDING_HERO_VIDEO_TIPS: 'Watch the full video',
 
   // 特性介绍
   LANDING_FEATURES_HEADER_1: '探索的过程',
