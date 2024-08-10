3d667c02fb8c7afbde5f6b4798c5f92aa786e7a4
baily <104406397+bailylu@users.noreply.github.com>
2024-08-10 13:24:43 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -17,7 +17,7 @@ const CONFIG = {
   STARTER_HERO_BUTTON_1_URL:
     'https://www.izanoffer.com/404', // 英雄区按钮
   STARTER_HERO_BUTTON_2_TEXT: '来论坛一起DIY吧', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_URL: 'https://blog.izanoffer.com', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
 
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
